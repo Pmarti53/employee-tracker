2d93cca4904ede1f8b8e6a15f0222034aec4a603
peter <ptmartinezsound@gmail.com>
2021-05-05 17:03:38 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,26 @@
 DROP DATABASE IF EXISTS manager;
+CREATE DATABASE manager;
+USE manager;
 
 
+-- CREATE TABLE Department (
+--     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--     dept_name VARCHAR(20) NOT NULL UNIQUE
+-- );
 
-CREATE TABLE Department (
-    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    dept_name VARCHAR(20) NOT NULL UNIQUE,
-);
+-- CREATE TABLE Role (
+--     id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--     title VARCHAR(30),
+--     salary DECIMAL,
+--     department_id INTEGER NOT NULL
+-- );
 
-CREATE TABLE Role (
-    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INTEGER AUTO_INCREMENT NOT NULL,
-);
-
-CREATE TABLE Employee (
-    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER
-);
+-- CREATE TABLE Employee (
+--     id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--     first_name VARCHAR(20) NOT NULL,
+--     last_name VARCHAR(20) NOT NULL,
+--     role_id INTEGER NOT NULL,
+--     manager_id INTEGER
+-- );
     
 
