94158d9012f82913cd4975773a632f8ab4563288
peter <ptmartinezsound@gmail.com>
2021-05-05 21:45:34 -0500

    added inquierer prompt

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,35 +5,35 @@ INSERT INTO Department (id, dept_name)
         (3, "Finance"),
         (4, "Legal");
 
--- INSERT INTO Role (id, title, salary, department_id)
---     VALUES
---     ("Sales Lead" , 40000, 1),
---     ("Salesperson", 25000, 1),
---     ("Lead Engineer", 100000, 2),
---     ("Software Engineer", 70000, 2),
---     ("Account Manager", 100000, 3),
---     ("Accountant", 90000, 3),
---     ("Legal Team Lead", 250000, 4),
---     ("Lawyer", 190000, 4);
+INSERT INTO Role (id, title, salary, department_id)
+    VALUES
+    (1, "Sales Lead" , 40000, 1),
+    (2, "Salesperson", 25000, 1),
+    (3, "Lead Engineer", 100000, 2),
+    (4, "Software Engineer", 70000, 2),
+    (5, "Account Manager", 100000, 3),
+    (6, "Accountant", 90000, 3),
+    (7, "Legal Team Lead", 250000, 4),
+    (8, "Lawyer", 190000, 4);
 
--- INSERT INTO Manager (id, first_name, last_name)
---     VALUES
---         ('Brett', 'Favre'),
---         ('Pennington', 'Chad'),
---         ('Mark', 'Brunell'),
---         ('Boomer', 'Esiason'),
+INSERT INTO Manager (id, first_name, last_name)
+    VALUES
+        (1, 'Brett', 'Favre'),
+        (2,'Pennington', 'Chad'),
+        (3, 'Mark', 'Brunell'),
+        (4, 'Boomer', 'Esiason');
 
 
--- INSERT INTO Employee (id, first_name, last_name, role_id, manager_id)
---     VALUES
---     ('Chad', 'Pennington', 1, 1),
---     ('Doug', 'Flutie', 2, 2),
---     ('Brett', 'Favre', 3, 0),
---     ('Peyton', 'Manning', 4, 1),
---     ('Mark', 'Brunell', 5, 0),
---     ('Steve', 'McNair', 6, 0),
---     ('Boomer', 'Esiason', 7, 3),
---     ('Dante', 'Culpepper', 9, 4)
+INSERT INTO Employee (id, first_name, last_name, role_id, manager_id)
+    VALUES
+    (1, 'Chad', 'Pennington', 1, 1),
+    (2, 'Doug', 'Flutie', 2, 2),
+    (3, 'Brett', 'Favre', 3, 0),
+    (4, 'Peyton', 'Manning', 4, 1),
+    (5, 'Mark', 'Brunell', 5, 0),
+    (6, 'Steve', 'McNair', 6, 0),
+    (7, 'Boomer', 'Esiason', 7, 3),
+    (8, 'Dante', 'Culpepper', 9, 4);
 
 
 
--- server.js
+++ server.js
@@ -18,40 +18,40 @@ const db = mysql.createConnection(
     console.log('Connected to the manager database.'),
 );
 
-// function mainPrompt() {
-//     inquirer.prompt({
-//         name: "WhatDo",
-//         type: "rawlist",
-//         Message: "What would you like to do?",
-//         choices: [
-//             {
-//                 name: "Add Department",
-//                 value: "Add_Department"
-//             },
-//             {
-//                 name: "Add Title",
-//                 value: "Add_Title"
-//             },
-//             {
-//                 name: "Add Employee",
-//                 value: "Add_Employee"
-//             },
-//             {
-//                 name: "View All Departments",
-//                 value: "View_Departments"
-//             },
-//             {
-//                 name: "View All Titles",
-//                 value: "View_Titles"
-//             },
-//             {
-//                 name: "View All Employees",
-//                 value: "View_Employees"
-//             }
-//         ]
-//     })
+function mainPrompt() {
+    inquirer.prompt({
+        name: "WhatDo",
+        type: "rawlist",
+        Message: "What would you like to do?",
+        choices: [
+            {
+                name: "Add Department",
+                value: "Add_Department"
+            },
+            {
+                name: "Add Title",
+                value: "Add_Title"
+            },
+            {
+                name: "Add Employee",
+                value: "Add_Employee"
+            },
+            {
+                name: "View All Departments",
+                value: "View_Departments"
+            },
+            {
+                name: "View All Titles",
+                value: "View_Titles"
+            },
+            {
+                name: "View All Employees",
+                value: "View_Employees"
+            }
+        ]
+    })
 
-// };
+};
 
 
 
@@ -64,5 +64,5 @@ const db = mysql.createConnection(
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    // mainPrompt();
+    mainPrompt();
 });
